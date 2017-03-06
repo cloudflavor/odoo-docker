030d7a6442729df0dbf48d4302c4c2d4a8b272cf
PI-Victor <palade.ionut@gmail.com>
2017-03-06 21:49:39 +0100

    replace with service env vars for kube

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ set -e
 
 # set the postgres database host, port, user and password according to the environment
 # and pass them as arguments to the odoo process if not present in the config file
-: ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
-: ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
-: ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
-: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
+#: ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
+#: ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
+#: ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
+#: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
 
 DB_ARGS=()
 function check_config() {
@@ -18,8 +18,8 @@ function check_config() {
         DB_ARGS+=("${value}")
    fi;
 }
-check_config "db_host" "$HOST"
-check_config "db_port" "$PORT"
+check_config "db_host" "$ODOO_DB_SERVICE_HOST"
+check_config "db_port" "$ODOO_DB_SERVICE_PORT"
 check_config "db_user" "$USER"
 check_config "db_password" "$PASSWORD"
 
